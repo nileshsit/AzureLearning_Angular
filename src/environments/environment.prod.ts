7e190a5a3c2521516af37e71aee51c9d357c4a5c
Chirag Patel <chirag.p@shaligraminfotech.com>
2025-11-19 13:48:39 +0530

    Added prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  baseUrl : 'https://azureleaningapi-fcdedph0cdc8fcc6.centralindia-01.azurewebsites.net/api/v1/'
 };
